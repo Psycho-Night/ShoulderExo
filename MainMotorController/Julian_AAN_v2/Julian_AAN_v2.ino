1ce8d5935a0a8d1319aa0244e2626767cf52d2fd
Psycho-Night <julekww@op.pl>
2025-09-30 14:14:12 +0200

    New tuned AAN and Interface for it

--- MainMotorController/Julian_AAN_v2/Julian_AAN_v2.ino
+++ MainMotorController/Julian_AAN_v2/Julian_AAN_v2.ino
@@ -1,22 +1,25 @@
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-// AAN controller - Maxon Current controller v1.0 Written for Arduino Due
+// AAN controller - Maxon Current controller
 
+// Written for Arduino Due
+
+// v2.0 - Additional closed loop current control
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 //--------------------- AAN ------------------------------------------------------------
 
-const float alpha = 2.0;  // woriking 2.0 or 3.5 if Freqency is 100Hz
+const float alpha = 3.5;  // 3.5
 const float beta = 0.05;  // Keep it 0.05
 
 
 // Tune those and keep changes
-const float a = 2.0;      // working 2.0 or 3.5 if Freqency is 100Hz
+const float a = 3.5;      // 3.5
 const float b = 5.0;      // Keep it 5.0
 
-float error = 0;          // Position error
-float error_prev = 0;     // Previous position error
-float error_vel = 0;      // Velocity error
+float error_angle = 0;          // Position error
+float error_prev_angle = 0;     // Previous position error
+float error_vel_angle = 0;      // Velocity error
 
 float epsilon = 0;
 float betta = 0;
@@ -26,11 +29,25 @@ float T_ff = 0;           // Feed forward torque
 float T_ff_prev = 0;      // Previous feed forward torque 
 float T_fb = 0;           // Feedback torque
 
+//----------- PID ---------------------------------------------------------------
+// Keep track of changes
+
+const float K_p = 1.0; // 0.8
+const float K_i = 0.0; // 0.02
+const float K_d = 0.0; // 0.01
+
+float error_current = 0.0;
+float error_vel_current = 0.0;
+float error_prev_current = 0.0;
+float error_int = 0.0;
+
+float PID_out = 0.0;
+
 //----------- SIN WAVE ---------------------------------------------------------------
 
 const float frequency = 0.05;            // Frequency in Hz (adjust as needed)
-const float amplitude = 35.0;           // Amplitude (max deviation from midpoint)
-const float offset = 35.0;              // Offset/midpoint of the sine wave
+const float amplitude = 40.0;           // Amplitude (max deviation from midpoint)
+const float offset = 40.0;              // Offset/midpoint of the sine wave
 const long InitialisationDelay = 2000;  // Delay used for initialisation
 const float phaseShift = -PI/2;          // Phase shift for graduate raise from 0 deg -PI/2 if sin wave and 0 if step
 
@@ -47,8 +64,10 @@ const int motorTourqePin = 6;       // PWM Set Value (Analog Input 1)
 
 // Sensors
 const int ANG_pin = A0;             // Shaft encoder pin def A0
-const int ESCON_OUT_pin = A8;               // ESCON speed output
+const int ESCON_OUT_pin = A8;       // ESCON speed output
 
+//------------- Parameters -----------------------------------------------------------
+// Motor
 const float V_max = 3.3;            // Max command voltage (e.g. driver input 0-5V)
 const int maxPWM = 4095;            // 12-bit PWM range
 const int PWM_deadZone = 33;
@@ -56,21 +75,29 @@ float desiredCurrent = 0;           // Desired current in A
 const float maxCurrent = 4.24;      // Max allowed current for driver
 const float Kt = 21.3/1000;         // Torque constany [Nm/A]
 
-//------------- Parameters -----------------------------------------------------------
-
-float targetAngle = 0;
-float targetPWM = 0;
-const float deg2rad = PI/180;               // Degrees to rad
-float targetAngleRad = targetAngle*deg2rad; // Target anlge in rads
-
+// AAN
 float currentAngle = 0;
 float currentAngleRad = 0;
 float currentAngle_prev = 0;
 float currentAngleRad_prev = 0;
 
+// PID
+float targetTourque = 0;
+float targetCurrent = 0;
+float targetPWM = 0;
+
+float currentTorque = 0;
+float currentTorque_prev = 0;
+
 float ActualCurrent = 0;
 
 
+// Encoder
+float targetAngle = 0;
+const float deg2rad = PI/180;               // Degrees to rad
+float targetAngleRad = targetAngle*deg2rad; // Target anlge in rads
+
+//  Exoskeleton
 const int gearRatio = 70;
 
 const float mass = 1.88; // kg
@@ -89,6 +116,7 @@ float Torque_old = 0;
 float up_bound = 0.01;    // upper bound for deadzone
 float down_bound = -0.01; // lower bound for deadzone
 
+
 void setup() {
   Serial.begin(115200);  // Serial Monitor
 
@@ -99,23 +127,22 @@ void setup() {
   pinMode(ANG_pin, INPUT);
   pinMode(ESCON_OUT_pin, INPUT);
 
-  digitalWrite(motorEnablePin, HIGH);
-  digitalWrite(motorDirectionPin, LOW);
-  analogWrite(motorTourqePin, 0);
-
   analogReadResolution(12);
   analogWriteResolution(12);
+
+  digitalWrite(motorEnablePin, HIGH);
+  digitalWrite(motorDirectionPin, LOW);
+  analogWrite(motorTourqePin, 0);  
 
   targetAngle = EncoderAngle();
   targetAngleRad = targetAngle*deg2rad;
   currentAngle = EncoderAngle();
   currentAngleRad = currentAngle*deg2rad;
-  error = targetAngle-currentAngle;
+  error_angle = targetAngle-currentAngle;
 
 }
 
 void loop() {
-
   // Time
   long currentTime = millis(); // [ms]
   float deltaTime = (currentTime - previousTime) / 1000.0; // [s]
@@ -123,13 +150,13 @@ void loop() {
 
   // Target
   if (currentTime<InitialisationDelay) { // Modify to start faster
-    targetAngle = EncoderAngle();
+    targetAngle = EncoderAngle(); //
     targetAngleRad = targetAngle * deg2rad;
    }
   else {
     long SinTime = currentTime - InitialisationDelay;
     targetAngle = offset + amplitude * sin(2.0 * PI * frequency * (SinTime/1000.0) + phaseShift); // Sine wave
-    targetAngleRad = targetAngle * deg2rad;
+    targetAngleRad = targetAngle * deg2rad;  
   }
 
   // Read and filter angle read
@@ -138,68 +165,81 @@ void loop() {
   currentAngleRad = currentAngle * deg2rad; // [rad]
 
   // Filter derivative
-  error = targetAngleRad - currentAngleRad; // [rad]
+  error_angle = targetAngleRad - currentAngleRad; // [rad]
 
-  float raw_derivative = (error - error_prev) / deltaTime; // [rad/s]
-  error_vel = 0.9 * error_vel + 0.1 * raw_derivative;  // smoothed derivative
+  float raw_derivative = (error_angle - error_prev_angle) / deltaTime; // [rad/s]
+  error_vel_angle = 0.9 * error_vel_angle + 0.1 * raw_derivative;  // smoothed derivative
 
   // ------------------- AAN Controller ----------------------------------
   
   //  Torque - Feed forward (FF)
-  T_ff = T_ff_prev + alpha*error_prev;
+  T_ff = T_ff_prev + alpha*error_prev_angle;
   
   //  Torque - Feedback (FB)
+  // float ep_t = error+beta*error_vel;
+  // float beta_t = a/(1+b*ep_t*ep_t);
+  // float f_t = ep_t/beta_t;
 
-  epsilon = error + beta*error_vel;
+  epsilon = error_angle + beta*error_vel_angle;
   betta   = a/(1+b*epsilon*epsilon);
   f = epsilon/betta;
+  // Serial.print(epsilon);
+  // Serial.print(" ");
+  // Serial.print(betta);
+  // Serial.print(" ");
+  // Serial.println(f);
+
 
   // Gain calculation
-  float K_p = f*error;
-  float K_d = f*error_vel;
+  float K_p = f*error_angle;
+  float K_d = f*error_vel_angle;
 
   // Torque calculation
-  T_fb = K_p*error+K_d*error_vel;
+  T_fb = K_p*error_angle+K_d*error_vel_angle;
 
-  // Deadzone mitigation
-  Torque_old = -(T_ff+T_fb); // Desired torque [Nm]
-  Torque = alpha_out*Torque_old+signum(Torque_old)*up_bound; // Filtered torque [Nm]
+  targetTourque = -(T_ff+T_fb);
 
-  // ------------------- Send Value ----------------------------------
+  // ------------------- PID Controller ----------------------------------
 
+  // Read values from ESCON
+  ActualCurrent = analogRead(ESCON_OUT_pin);
+  ActualCurrent = ActualCurrent/4095*4.24;
 
-  desiredCurrent = (abs(Torque)/gearRatio) / Kt; // Convertion from Torque to Current [A]
+  targetCurrent = (abs(targetTourque)/gearRatio) / Kt; // Convertion from Torque to Current [A]
 
-  if (Torque>=0){
+  error_current = targetCurrent - ActualCurrent;
+  // Serial.println(error);
+
+  error_vel_current = (error_current-error_prev_current)/deltaTime;
+  // Serial.println(error_d);
+  error_int += error_current;
+  // Serial.println(error_int);
+  PID_out = K_p*error_current+K_d*error_vel_current+K_i*error_int;
+
+  if (targetTourque>=0){
     digitalWrite(motorDirectionPin,LOW);
   }
   else {
     digitalWrite(motorDirectionPin,HIGH);
   }
   
-  DesiredPWM= constrain((desiredCurrent/maxCurrent) * maxPWM, 0, maxPWM);
+  DesiredPWM = constrain((abs(PID_out)/maxCurrent) * maxPWM, 0, maxPWM);
 
   analogWrite(motorTourqePin, DesiredPWM);
   digitalWrite(motorEnablePin, HIGH);
 
-  // Read values from ESCON
-  ActualCurrent = analogRead(ESCON_OUT_pin);
-  ActualCurrent = ActualCurrent/4095*4.24;
-  // ActualCurrent = ActualCurrent/1023*4.24;
-
-  error_prev = error;
   previousTime = currentTime;
+  error_prev_angle = error_angle;
+  error_prev_current = error_current;
 
-  // ------------------- Send COM Message ----------------------------------
-  
   // COM message for plotting 
   String output = "Time: " + String(currentTime) + 
                 "ms, Target Angle: " + String(targetAngle) +
                 ", Current Angle: " + String(currentAngle) +
                 ", T_FF: " + String(T_ff)+
                 ", T_FB: " + String(T_fb)+
-                ", Torque: " + String(Torque)+
-                ", Target Current: " + String(desiredCurrent) +
+                ", Torque: " + String(targetTourque)+
+                ", Target Current: " + String(targetCurrent) +
                 ", Current: " + String(ActualCurrent) +
                 ", PWM: " + String(DesiredPWM)+
                 ", sampling Frequency: " + String(Frequency);
@@ -208,45 +248,20 @@ void loop() {
               //  ", kp: " + String(Kp);
               //  ", kd: " + String(Kd);
                
-  Serial.println(output);
-
-  // --------------- DEBUG ---------------------------------------------------
-  // Serial.print(T_ff);
-  // Serial.print(" ");
-  // Serial.print(T_ff_prev);
-  // Serial.print(" ");
-  // Serial.print(T_fb);
-  // Serial.print(" ");
-  // Serial.println(Torque);
-  // Serial.print(" ");
+Serial.println(output);
 
 
 
 
 }
 
-float signum(float sig_input){
-  if (sig_input>0) {
-  float sig_out = 1.0;
-  return sig_out;
-  }
-  else if (sig_input<0) {
-  float sig_out = -1.0;
-  return sig_out;
-  }
-  else {
-  float sig_out = 0;
-  return sig_out;
-  }
-  
-}
+
 
 float EncoderAngle(){
   // Read encoder 
   int sensorValue = analogRead(ANG_pin);
   // Calculate the angle
-  // float readAngle = -0.3315*sensorValue + 200.9-20;
-  float readAngle = -0.0845*sensorValue + 181.16;
+  float readAngle = -0.0845*sensorValue+181.16;
   // float readAngle = sensorValue; // Change to this line for calibration
   return readAngle;
 }
